3114e7a2a4d6ffdf876d01a82c602657aa90bb41
Hanzlah11 <hanzlah.imran402@gmail.com>
2026-05-07 09:01:30 +0500

    New level

--- assets/data/tilesets/inside.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="inside" tilewidth="16" tileheight="16" tilecount="1536" columns="48">
- <image source="C:/Users/Hanzlah/Downloads/Super-Pirate-World-main/graphics/tilesets/inside.png" width="768" height="512"/>
-</tileset>
